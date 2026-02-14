16bd1a60f573316a6c589c4bce03d1aa380fee0d
Nizartio Candra Adinata <nizartiochandraadianta@gmail.com>
2026-02-14 11:36:35 +0700

    feat(frontend): added search, filter, and navigation

--- src/api/adminBookingApi.ts
+++ src/api/adminBookingApi.ts
@@ -1,4 +1,5 @@
 import api from "./apiClient"
+import type { PaginatedBookingResponse } from "../types/admin";
 
 export const fetchPendingBookings = async () => {
   const response = await api.get(
@@ -7,13 +8,33 @@ export const fetchPendingBookings = async () => {
   return response.data;
 };
 
+
+export const fetchAdminBookings = async (
+  page: number,
+  status?: string,
+  search?: string
+): Promise<PaginatedBookingResponse> => {
+
+  let query = `?page=${page}&pageSize=10`;
+
+  if (status) {
+    query += `&status=${status}`;
+  }
+
+  if (search && search.trim() !== "") {
+    query += `&search=${encodeURIComponent(search)}`;
+  }
+
+  const response = await api.get<PaginatedBookingResponse>(`/room-bookings${query}`);
+
+  return response.data;
+};
+
 export const updateBookingStatus = async (
   id: number,
   status: "Approved" | "Rejected"
 ) => {
-  const response = await api.put(
-    `/room-bookings/${id}/status`,
-    { status }
-  );
-  return response.data;
+  return api.put(`/room-bookings/${id}/status`, {
+    status
+  });
 };
